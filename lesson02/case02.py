f7a25696c7ab72ac3a0ed78d61808a376dfd6001
ym210 <ym210676303@hotmail.com>
2020-04-15 19:38:31 +0800

    lesson2

---
+++ lesson02/case02.py
@@ -0,0 +1,14 @@
+# -*- coding:UTF-8 -*-
+
+#轉型態
+num1 = 10 + 3.14
+print(type(num1))
+num2 = 5 + True
+print(num2)
+print(type(num2))
+
+a = "100"
+b = "90.5"
+print(a + b, type(a + b))
+print(int(a) + float(b))
+
