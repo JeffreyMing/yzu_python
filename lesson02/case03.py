1880b5bf658bc56fd3785035e0c383d2521c3565
ym210 <ym210676303@hotmail.com>
2020-04-15 19:57:32 +0800

    lesson2

--- lesson02/case03.py
+++ lesson02/case03.py
@@ -2,16 +2,19 @@
 
 import random
 
-#隨機亂數
-n = random.randrange(0,2)
+# 隨機亂數
+n = random.randrange(0, 2)
 print(n)
 
-n = random.randint(1,100)
+n = random.randint(1, 100)
 print(n)
 
-#電腦選號四星彩
+# 電腦選號四星彩
 n1 = random.randint(0, 9)
 n2 = random.randint(0, 9)
 n3 = random.randint(0, 9)
 n4 = random.randint(0, 9)
 print(n1, n2, n3, n4)
+
+#跳脫字元
+print("\"")
