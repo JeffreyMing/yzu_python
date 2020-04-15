43cb5f4c2e6c703e481867a67cadc454e84ff7f9
ym210 <ym210676303@hotmail.com>
2020-04-15 21:07:29 +0800

    lesson2

--- lesson02/case06.py
+++ lesson02/case06.py
@@ -1,11 +1,13 @@
 # -*- coding:UTF-8 -*-
 
-#補位數
+#補位數，先補小數後位數
 exchange = 29.123
 print('%09.5f' % exchange)
 exchange = 129.123
 print('%09.5f' % exchange)
 exchange = 9.123
+print('%09.5f' % exchange)
+exchange = 7884512339.123
 print('%09.5f' % exchange)
 
 a = 198
