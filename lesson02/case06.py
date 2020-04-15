f12b83f390a36302b423db27d31fbce8aedd2722
ym210 <ym210676303@hotmail.com>
2020-04-15 21:04:26 +0800

    lesson2

---
+++ lesson02/case06.py
@@ -0,0 +1,12 @@
+# -*- coding:UTF-8 -*-
+
+#補位數
+exchange = 29.123
+print('%09.5f' % exchange)
+exchange = 129.123
+print('%09.5f' % exchange)
+exchange = 9.123
+print('%09.5f' % exchange)
+
+a = 198
+print('%09d' % a)
