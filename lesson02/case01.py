a4568ad0e31254610bd4a589ac7787169bf1df7b
ym210 <ym210676303@hotmail.com>
2020-04-15 19:16:14 +0800

    lesson2

---
+++ lesson02/case01.py
@@ -0,0 +1,39 @@
+# -*- coding:UTF-8 -*-
+
+print("我是中文")
+if 2 < 0:
+    print("A")
+    print("B")
+else:
+    print("C")
+    print("D")
+print("E")
+
+# a = 1
+# b = 3
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+a = 100
+print(a)
+b = 0b101  #二進位
+print(b)
+c = 0o12  #八進位
+print(c)
+d = 0x12  #十六進位
+print(d)
+
+#數字型態
+a = 10
+print(type(a))
+b = 3.14
+print(type(b))
+c = 1 + 2j
+print(type(c))
+
+#變數
+age, name = 18, "Jeff"
+print(age, name)
+
