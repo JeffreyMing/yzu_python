36b281d183065cbfb56d38d50903a031bd3fcaa7
ym210 <ym210676303@hotmail.com>
2020-04-22 18:52:37 +0800

    lesson 3

--- lesson03/case02.py
+++ lesson03/case02.py
@@ -7,5 +7,10 @@ else:
     print('%d 奇數' % n)
 
 # 類三元運算子寫法
-
 print('%d %s' % (n, "偶數" if n % 2 == 0 else "奇數"))
+
+# 定義函數，調用函數使用
+def isOdd(n):
+    return '偶數' if n % 2 == 0 else '奇數'
+
+print('%d %s' % (n, isOdd(n)))
