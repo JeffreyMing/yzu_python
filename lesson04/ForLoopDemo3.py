992ca1b6481838f25b490176c6848dfda4c89ecb
ym210 <ym210676303@hotmail.com>
2020-04-29 19:07:49 +0800

    lesson 4

--- lesson04/ForLoopDemo3.py
+++ lesson04/ForLoopDemo3.py
@@ -6,8 +6,16 @@ e3 = {'name': 'Bob', 'salary': 70000}
 emps = [e1, e2, e3]
 print(emps)
 
-#求薪資總和
+# 求薪資總和
 sum = 0
 for n in emps:
     sum += n['salary']
 print(sum)
+
+# Solution2
+salary = [] # 建立一個數組
+for n in emps:
+    salary.append(n['salary'])
+print(salary)
+print(max(salary))
+print(min(salary))
