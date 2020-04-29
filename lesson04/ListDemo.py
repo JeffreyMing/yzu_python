3fd051f7e6fa44de1601f69ac717a9c75799ca53
ym210 <ym210676303@hotmail.com>
2020-04-29 19:52:02 +0800

    lesson 4

--- lesson04/ListDemo.py
+++ lesson04/ListDemo.py
@@ -1,4 +1,5 @@
 import operator
+import random as r
 poker1 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 poker2 = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
@@ -7,3 +8,29 @@ print(operator.eq(poker1, poker2))
 poker = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] * 4
 print(len(poker))
 print(poker.count('A'))
+
+# 抽第一張牌
+card = poker.pop(0)
+print('抽第一張牌', card)
+print(poker)
+
+# 洗牌
+r.shuffle(poker)
+print(poker)
+
+# 計算前3張的點數, A=1, 2=2 ... J,Q,K=0.5
+card = []
+def score(po):  # 假設po = ['A', 9, 'J']
+    sum = 0
+    for p in po:
+        if p == 'A':
+            sum = sum + 1
+            continue
+        if p == 'J' or p == 'Q' or p == 'K':
+            sum = sum + 0.5
+            continue
+        sum += p
+    return sum
+
+print(poker[:3])
+print(score(poker[:3]))
