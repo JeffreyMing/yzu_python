b9b1e22d29c24289ab82e9a24d54f94fed401bb6
ym210 <ym210676303@hotmail.com>
2020-04-29 19:02:20 +0800

    lesson 4

---
+++ lesson04/ForLoopDemo3.py
@@ -0,0 +1,13 @@
+# 複合結構
+e1 = {'name': 'John', 'salary': 50000}
+e2 = {'name': 'Mary', 'salary': 60000}
+e3 = {'name': 'Bob', 'salary': 70000}
+
+emps = [e1, e2, e3]
+print(emps)
+
+#求薪資總和
+sum = 0
+for n in emps:
+    sum += n['salary']
+print(sum)
