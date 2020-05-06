531dc146958e1230f7a106f3c50c4e7a94afdd82
ym210 <ym210676303@hotmail.com>
2020-05-06 21:22:46 +0800

    lesson 5

--- lesson05/JsonDemo2.py
+++ lesson05/JsonDemo2.py
@@ -1,10 +1,13 @@
 # 市售食米抽檢不合格產品
 # https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx
+# 市售食米抽檢合格產品
+# https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx
 
 import json
 import requests
 
-url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+#url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx'
 
 r = requests.get(url)
 
@@ -13,4 +16,5 @@ r = requests.get(url)
 
 bad_rices = json.loads(r.text)
 for bad in bad_rices:
-    print(bad.get('品名'))
+    if '月光' in bad.get('品名'):
+        print(bad.get('品名'))
