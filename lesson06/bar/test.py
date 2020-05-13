4c1ce4d44eed9eacbf36c2c5c3285732cf2714ae
ym210 <ym210676303@hotmail.com>
2020-05-13 19:57:39 +0800

    lesson 5

---
+++ lesson06/bar/test.py
@@ -0,0 +1,13 @@
+from lesson06.bar import *
+
+print(os)
+print(sys.platform)
+print(platform.system())
+print(copyright())
+
+
+print(random())
+print(randint(1, 50))
+print(randrange(1, 10))
+lan = ["java", "python", "C#", "swift"]
+print(choice(lan))
