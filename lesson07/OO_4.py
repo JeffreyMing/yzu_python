25052560647ff6f7434375d84e07bfb05ff117e0
ym210 <ym210676303@hotmail.com>
2020-05-20 19:43:39 +0800

    lesson 7

---
+++ lesson07/OO_4.py
@@ -0,0 +1,19 @@
+class Father:
+    money = 0
+    def __init__(self, money):
+        self.money = money
+    def __str__(self):
+        return "money = " + str(self.money)
+
+class Son(Father): # 繼承father
+    age = 0
+    def __init__(self, money, age):
+        Father.__init__(self, money)
+        self.age = age
+    def __str__(self):
+        return Father.__str__(self) + " son age = " + str(Son.age)  #呼叫father的__str__
+
+if __name__ == '__main__':
+    son = Son(50000, 10)
+    print(son.age, son.money)
+    print(son)
