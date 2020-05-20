602320922dc1df027c34cf0548d885e4a08d3fc6
ym210 <ym210676303@hotmail.com>
2020-05-20 19:53:07 +0800

    lesson 7

---
+++ lesson07/OO_5.py
@@ -0,0 +1,20 @@
+#多重繼承
+class Engine:
+    power = 0
+
+class Wheel:
+    count = 0
+
+class Car(Engine, Wheel): # 繼承Engine、Wheel
+    def __init__(self, price, power, count):
+        self.price = price
+        self.power = power
+        self.count = count
+
+    def __str__(self):
+        return str(self.price) + ", " + str(self.power) +", " +  str(self.count)
+
+
+if __name__ == '__main__':
+    car = Car(100000, 150, 2)
+    print(car)
