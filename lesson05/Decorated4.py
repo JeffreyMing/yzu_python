06a60460b2c70e844d5e7990aa31eb754193ca4c
ym210 <ym210676303@hotmail.com>
2020-05-20 18:42:17 +0800

    lesson 5

---
+++ lesson05/Decorated4.py
@@ -0,0 +1,18 @@
+def login(password):
+    def decorated(func):
+        def check():
+            if password == 1234:
+                print("登入成功")
+                func()
+            else:
+                print("登入失敗")
+                return None
+        return check
+    return decorated
+
+@login(password=1234)
+def report():
+    print("密件: 今日頭條...")
+
+
+report()
