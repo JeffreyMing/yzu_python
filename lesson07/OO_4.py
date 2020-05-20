66bf45cc0520b3c3ea29f07dfb040d2fb824c496
ym210 <ym210676303@hotmail.com>
2020-05-20 19:45:29 +0800

    lesson 7

--- lesson07/OO_4.py
+++ lesson07/OO_4.py
@@ -8,7 +8,7 @@ class Father:
 class Son(Father): # 繼承father
     age = 0
     def __init__(self, money, age):
-        Father.__init__(self, money)
+        Father.__init__(self, money) # 在子類別初始化後可繼承父類別的money
         self.age = age
     def __str__(self):
         return Father.__str__(self) + " son age = " + str(Son.age)  #呼叫father的__str__
