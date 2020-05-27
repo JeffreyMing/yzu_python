eb9901ed21951d58ff561681cb93a49c756fe97c
ym210 <ym210676303@hotmail.com>
2020-05-27 20:29:28 +0800

    lesson 8

---
+++ lesson08/GUI/GUIDemo3.py
@@ -0,0 +1,20 @@
+import tkinter
+from tkinter import messagebox
+
+def mymessage():
+    messagebox.showinfo('Message', 'Hello')
+
+def myexit():
+    win.quit()
+
+win = tkinter.Tk()
+win.title('我的小視窗')
+win.geometry('300x300')
+
+button = tkinter.Button(win, text='OK', command=mymessage)
+button.pack()
+
+button2 = tkinter.Button(win, text='Exit', command = myexit)
+button2.pack()
+
+win.mainloop()
