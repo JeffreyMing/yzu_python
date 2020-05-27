2111c87202caa47031b0ffc66ab3392f050cba20
ym210 <ym210676303@hotmail.com>
2020-05-27 19:05:11 +0800

    lesson 8

--- lesson08/ExceptDemo3.py
+++ lesson08/ExceptDemo3.py
@@ -9,6 +9,8 @@ def input_number():
     except ValueError as e:
         print('資料輸入錯誤', e)
         input_number()
+    except Exception as e:
+        print('發生了一個料想不到的錯誤', e)
     else:
         print(z)
 
