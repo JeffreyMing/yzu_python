aecc84f7ff36a313ae0b9b756704f3971b0fb082
ym210 <ym210676303@hotmail.com>
2020-05-27 18:50:39 +0800

    lesson 8

--- lesson08/ExceptDemo1.py
+++ lesson08/ExceptDemo1.py
@@ -1,7 +1,14 @@
+import sys
+import traceback
+
 x = 10
 y = int(input('請輸入數字:'))
 try:
     z = x / y
     print(z)
-except:
-    print('有錯誤發生')
+except Exception as e: # 顯示錯誤原因
+    print('有錯誤發生', e)
+    print(e.__class__.__name__)
+    cl, exc, tb = sys.exc_info()
+    lastCallStack = traceback.extract_tb(tb)[-1] # 詳細錯誤位置
+    print(lastCallStack)
