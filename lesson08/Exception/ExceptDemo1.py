daff253fb03d6fc6828b45d23b293e4ffbe7b634
ym210 <ym210676303@hotmail.com>
2020-05-27 19:43:10 +0800

    lesson 8

--- lesson08/Exception/ExceptDemo1.py
+++ lesson08/Exception/ExceptDemo1.py
@@ -5,10 +5,11 @@ x = 10
 y = int(input('請輸入數字:'))
 try:
     z = x / y
-    print(z)
 except Exception as e: # 顯示錯誤原因
     print('有錯誤發生', e)
     print(e.__class__.__name__)
     cl, exc, tb = sys.exc_info()
     lastCallStack = traceback.extract_tb(tb)[-1] # 詳細錯誤位置
     print(lastCallStack)
+else:
+    print(z)
